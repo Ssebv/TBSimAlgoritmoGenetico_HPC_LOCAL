dab4632725df5381fef7472518ef047b3d0c37af
Ssebv <sallendec@uft.edu>
2023-12-17 20:28:49 -0300

    Analisis

--- analisis/analisis_JENETICS.py
+++ analisis/analisis_JENETICS.py
@@ -4,41 +4,42 @@ import matplotlib.pyplot as plt
 # Carga los datos desde el archivo CSV
 datos = pd.read_csv('../ResultadosAlgoritmoGenetico.csv')
 
+
+# Ajustes para los gráficos
+fig_size = (12, 8)  # Tamaño de la figura aumentado
+alpha = 0.7  # Transparencia
+
 # Graficar la aptitud del mejor individuo por generación
-plt.figure(figsize=(10, 6))
-plt.plot(datos['Generacion'], datos['Aptitud Mejor Individuo'], marker='o')
+plt.figure(figsize=fig_size)
+plt.plot(datos['Generacion'], datos['Aptitud Mejor Individuo'], alpha=alpha)
 plt.title('Aptitud del Mejor Individuo por Generación')
 plt.xlabel('Generación')
 plt.ylabel('Aptitud')
 plt.grid(True)
-plt.savefig('aptitud_por_generacion.png')
 
-# Graficar el tiempo por generación
-plt.figure(figsize=(10, 6))
-plt.plot(datos['Generacion'], datos['Tiempo por generacion'], marker='o', color='r')
-plt.title('Tiempo por Generación')
+# Graficar el tiempo por generación con suavizado
+plt.figure(figsize=fig_size)
+plt.plot(datos['Generacion'], datos['Tiempo por generacion'].rolling(window=5).mean(), color='r', alpha=alpha)
+plt.title('Tiempo por Generación (Promedio Móvil)')
 plt.xlabel('Generación')
-plt.ylabel('Tiempo (nanosegundos)')
+plt.ylabel('Tiempo (milisegundos)')
 plt.grid(True)
-plt.savefig('tiempo_por_generacion.png')
 
-# Graficar el tiempo acumulado por generación
-plt.figure(figsize=(10, 6))
-plt.plot(datos['Generacion'], datos['Tiempo total'], marker='o', color='g')
+# Graficar el tiempo total acumulado por generación
+plt.figure(figsize=fig_size)
+plt.plot(datos['Generacion'], datos['Tiempo total'], color='g', alpha=alpha)
 plt.title('Tiempo Acumulado por Generación')
 plt.xlabel('Generación')
-plt.ylabel('Tiempo Acumulado (nanosegundos)')
+plt.ylabel('Tiempo Acumulado (milisegundos)')
 plt.grid(True)
-plt.savefig('tiempo_acumulado_por_generacion.png')
 
-# Grafica Uso de CPU
-plt.figure(figsize=(10, 6))
-plt.plot(datos['Generacion'], datos['Uso CPU'], marker='o', color='y')
-plt.title('Uso de CPU')
+# Gráfico de dispersión para el uso de CPU
+plt.figure(figsize=fig_size)
+plt.scatter(datos['Generacion'], datos['Uso CPU'], color='y', alpha=alpha)
+plt.title('Uso de CPU por Generación')
 plt.xlabel('Generación')
 plt.ylabel('Uso de CPU (%)')
 plt.grid(True)
-plt.savefig('uso_cpu.png')
 
 
 
